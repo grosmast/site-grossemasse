b2c77b364fb176c15db911fe79698709b75d07f5
grosmast <themast253@mail.com>
2023-11-02 17:30:56 +0100

    ajout de l'index html

---
+++ Untitled-1.php
@@ -0,0 +1,84 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css1.css">
+</head>
+<body > 
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+  <header>
+    <div class="container">
+      <div class="logo">
+        <img src="madlogo.svg" alt="" />
+      <div class="onglet">
+        <ul>
+          <li><a href="#">Accueil</a></li>
+          <li><a href="#">Soumettre des Documents</a></li>
+          <li><a href="#">Crédit</a></li>
+          
+                 
+        </ul>
+      </div>
+      <div class="logo"></div>
+    </div>
+  </header>
+  <p class="hr"><hr SIZE="4"></p>
+  
+  <div class="liste">
+    <div class="container">
+      <ul>
+        <li><a href="#">bricolo</a></li>
+        <li><a href="#">coding</a></li>
+        <li><a href="#">crypto</a></li>
+        <li><a href="#">e-zines</a></li>
+        <li><a href="#">esprit</a></li>
+        <li><a href="#">reseau</a></li>
+        <li><a href="#">sysadm</a></li>
+        <li><a href="#">tech</a></li>
+        <li><a href="#">windoz</a></li>
+        <li><a href="#">lastupdates.txt</a></li>
+        <li><a href="#">links.html</a></li>
+        <li><a href="#">repositorium.html</a></li>
+        <li><a href="#">robots.txt</a></li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="container text-center">
+    <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+      <div class="col">
+        <div class="p-3">Row column</div>
+      </div>
+      <div class="col">
+        <div class="p-3">Row column</div>
+      </div>
+      <div class="col">
+        <div class="p-3">Row column</div>
+      </div>
+      <div class="col">
+        <div class="p-3">Row column</div>
+      </div>
+      <div class="col">
+        <div class="p-3">Row column</div>
+      </div>
+      <div class="col">
+        <div class="p-3">Row column</div>
+      </div>
+      <div class="col">
+        <div class="p-3">Row column</div>
+      </div>
+      <div class="col">
+        <div class="p-3">Row column</div>
+      </div>
+      <div class="col">
+        <div class="p-3">Row column</div>
+      </div>
+      <div class="col">
+        <div class="p-3">Row column</div>
+      </div>
+    </div>
+  </div>
+</body> 
